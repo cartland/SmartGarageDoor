584aa2df9ce1624943dea8726df22b5ea12b8701
cartland <c.cartland@gmail.com>
2021-03-02 23:43:09 -0800

    Add secret.h
    Remember to ask git to ignore this file when you use your secrets.
    
        git update-index --assume-unchanged secrets.h

---
+++ feather/FeatherESP32/secrets.h
@@ -0,0 +1,10 @@
+#pragma once
+
+// secrets.h
+
+// WiFi.
+#define WIFI_SSID "WIFI_SSID"
+#define WIFI_PASSWORD "WIFI_PASSWORD"
+
+// Open Weather Maps.
+#define URL "https://example.com/path?key1=value2&key2=value2"
