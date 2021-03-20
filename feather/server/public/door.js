91f2866fe1af57325fe2fad3e40f2d5eee78aeb8
cartland <c.cartland@gmail.com>
2021-03-19 20:48:50 -0700

    Add secondary sensor for basic error checking

--- feather/server/public/door.js
+++ feather/server/public/door.js
@@ -5,11 +5,13 @@ function getDoorStatus(observations, sensorClosedName, sensorOpenName) {
   const output = {
     closed: false,
     state: STATE_UNKNOWN,
-    message: '',
+    messages: '',
     lastUpdatedMillis: null,
   };
+  console.log(recent);
   const closedSensorHistory = getHistoryWithDurations(recent.sensorClosedData, nowMillis);
   if (closedSensorHistory.length <= 0) {
+    console.error('No closed sensor history');
     return output;
   }
   const confirmedClosed = closedSensorHistory[0].value == 0;
@@ -28,10 +30,36 @@ function getDoorStatus(observations, sensorClosedName, sensorOpenName) {
   output.messages = [
     'Last updated ' + lastUpdatedString,
   ];
+
+  const openSensorHistory = getHistoryWithDurations(recent.sensorOpenData, nowMillis);
+  console.log(openSensorHistory);
+  if (openSensorHistory.length <= 0) {
+    return output;
+  }
+  const confirmedOpen = openSensorHistory[0].value == 0;
+  if (confirmedOpen) {
+    if (confirmedClosed) {
+      output.state = STATE_ERROR_BOTH_SENSORS_ACTIVE;
+    } else {
+      // Confirmed closed.
+    }
+  } else {
+    if (confirmedClosed) {
+      // Confirmed open.
+    } else {
+      output.state = STATE_OPEN_UNCONFIRMED;
+    }
+  }
+  const openSensorLastUpdatedMillis = openSensorHistory[0].timeMilliseconds;
+  output.openSensorLastUpdatedMillis = openSensorLastUpdatedMillis;
   return output;
 }
 
 function getHistoryWithDurations(data, nowMillis) {
+  if (!data) {
+    console.error('getHistoryWithDurations: No data');
+    return [];
+  }
   const history = [];
   let timeMillis = nowMillis;
   for (const datum of data) {
