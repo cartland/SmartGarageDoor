3d771138030daf2f9230333cb1b0cb25e63760f7
cartland <c.cartland@gmail.com>
2021-04-04 00:58:20 -0700

    Add FirestoreDocumentReferenceLiveData

---
+++ feather/AndroidGarage/app/src/main/java/com/chriscartland/garage/FirestoreDocumentReferenceLiveData.kt
@@ -0,0 +1,77 @@
+/*
+ * Copyright 2021 Chris Cartland. All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+package com.chriscartland.garage
+
+import android.util.Log
+import androidx.lifecycle.LiveData
+import com.google.firebase.firestore.DocumentReference
+import com.google.firebase.firestore.DocumentSnapshot
+import com.google.firebase.firestore.EventListener
+import com.google.firebase.firestore.FirebaseFirestoreException
+import com.google.firebase.firestore.ListenerRegistration
+
+/**
+ * Based on:
+ * https://firebase.googleblog.com/2017/12/using-android-architecture-components.html
+ */
+class FirestoreDocumentReferenceLiveData(
+    documentReference: DocumentReference
+) : LiveData<DocumentSnapshot?>() {
+
+    private var listenerRegistration: ListenerRegistration? = null
+
+    private var documentReference: DocumentReference = documentReference
+        set(value) {
+            field = value
+            if (listenerRegistration != null) {
+                listenerRegistration?.remove()
+                listenerRegistration = value.addSnapshotListener(listener)
+            }
+        }
+
+    private val listener: EventListener<DocumentSnapshot?> = Listener(this)
+    private class Listener(
+        val liveData: LiveData<DocumentSnapshot?>
+    ) : EventListener<DocumentSnapshot?> {
+        override fun onEvent(value: DocumentSnapshot?, error: FirebaseFirestoreException?) {
+            if (error != null) {
+                Log.w(MainActivity.TAG, "Event listener failed.", error)
+            }
+            if (value != null && value.exists()) {
+                liveData.value = value
+            }
+        }
+    }
+
+    override fun onActive() {
+        super.onActive()
+        Log.d(TAG, "onActive")
+        listenerRegistration = documentReference.addSnapshotListener(listener)
+    }
+
+    override fun onInactive() {
+        super.onInactive()
+        Log.d(TAG, "onInactive")
+        listenerRegistration?.remove()
+        listenerRegistration = null
+    }
+
+    companion object {
+        val TAG = FirestoreDocumentReferenceLiveData::class.java.simpleName
+    }
+}
