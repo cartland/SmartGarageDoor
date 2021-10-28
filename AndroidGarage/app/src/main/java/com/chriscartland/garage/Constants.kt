6c8b9aa50272cee23f6b29d17916d27699246d46
cartland <c.cartland@gmail.com>
2021-10-28 00:15:10 -0700

    Update placeholder constants

--- AndroidGarage/app/src/main/java/com/chriscartland/garage/Constants.kt
+++ AndroidGarage/app/src/main/java/com/chriscartland/garage/Constants.kt
@@ -19,7 +19,7 @@ package com.chriscartland.garage
 
 class Constants {
     companion object {
-        // https://us-central1-PROJECT-ID.cloudfunctions.net/currentEventData\?session\=\&buildTimestamp\=Sat%20Mar%2013%2014%3A45%3A00%202021
+        // https://us-central1-PROJECT-ID.cloudfunctions.net/eventHistory?session=ABC&buildTimestamp=Sat%20Mar%2013%2014%3A45%3A00%202021
         const val SCHEME = "https"
         const val AUTHORITY = "us-central1-PROJECT-ID.cloudfunctions.net"
         const val CURRENT_EVENT_DATA_PATH = "currentEventData"
@@ -27,5 +27,11 @@ class Constants {
 
         const val SESSION_PARAM_KEY = "session"
         const val BUILD_TIMESTAMP_PARAM_KEY = "buildTimestamp"
+
+        const val CHECK_IN_THRESHOLD_SECONDS = 60 * 15 // 15 minutes in seconds.
+
+        // Before compiling this project, change the value of web_client_id in auth.xml.
+        // This copy of the incorrect value allows us to log a warning if auth.xml has not changed.
+        const val INCORRECT_WEB_CLIENT_ID = "123456789012-zzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzz.apps.googleusercontent.com"
     }
 }
