4cb38ccd6fc7525742322f75a464f70ee916a9ce
cartland <c.cartland@gmail.com>
2024-10-03 23:36:20 -0700

    Prepare Firebase auth

--- AndroidGarage2/app/src/main/java/com/chriscartland/garage/MainActivity.kt
+++ AndroidGarage2/app/src/main/java/com/chriscartland/garage/MainActivity.kt
@@ -7,7 +7,6 @@ import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.activity.viewModels
 import androidx.lifecycle.lifecycleScope
-import androidx.lifecycle.viewmodel.compose.viewModel
 import com.chriscartland.garage.fcm.updateOpenDoorFcmSubscription
 import com.chriscartland.garage.ui.GarageApp
 import com.chriscartland.garage.viewmodel.DoorViewModel
@@ -33,4 +32,56 @@ class MainActivity : ComponentActivity() {
             updateOpenDoorFcmSubscription(this@MainActivity, buildTimestamp)
         }
     }
+
+//    fun handleOneTapSignIn(activity: Activity, data: Intent?) {
+//        try {
+//            val oneTapClient = oneTapSignInClient ?: return
+//            val credential = oneTapClient.getSignInCredentialFromIntent(data)
+//            val idToken = credential.googleIdToken ?: return
+//            firebaseAuthWithGoogle(activity, idToken)
+//        } catch (e: ApiException) {
+//            Log.e(TAG, "ApiException: handleOneTapSignIn ${e.message}")
+//            when (e.statusCode) {
+//                CommonStatusCodes.CANCELED -> {
+//                    Log.d(TAG, "One-tap dialog was closed.")
+//                    // Don't re-prompt the user.
+//                    showOneTapUI.value = false
+//                }
+//                CommonStatusCodes.NETWORK_ERROR -> {
+//                    Log.d(TAG, "One-tap encountered a network error.")
+//                    showOneTapUI.value = true
+//                }
+//                else -> {
+//                    Log.d(
+//                        TAG, "Couldn't get credential from result." +
+//                                " ApiException.statusCode: ${e.statusCode}" +
+//                                " (${e.localizedMessage})")
+//                }
+//            }
+//        }
+//    }
+//
+//    private fun firebaseAuthWithGoogle(activity: Activity, idToken: String) {
+//        Log.d(TAG, "firebaseAuthWithGoogle")
+//        val credential = GoogleAuthProvider.getCredential(idToken, null)
+//        Firebase.auth.signInWithCredential(credential)
+//            .addOnCompleteListener(activity) { task ->
+//                if (task.isSuccessful) {
+//                    // Sign in success, update UI with the signed-in user's information
+//                    Log.d(TAG, "signInWithCredential:success")
+//                    val user = Firebase.auth.currentUser
+//                    firebaseUser.value = user
+//                } else {
+//                    // If sign in fails, display a message to the user.
+//                    Log.w(TAG, "signInWithCredential:failure", task.exception)
+//                    firebaseUser.value = null
+//                }
+//            }
+//    }
+//
+//    fun signOut() {
+//        Firebase.auth.signOut()
+//        oneTapSignInClient?.signOut()
+//        firebaseUser.value = null
+//    }
 }
