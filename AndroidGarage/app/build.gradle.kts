67d0900d3840fd5dc914a6ac7c847dfe7e358b4b
cartland <c.cartland@gmail.com>
2024-11-11 23:05:49 -0800

    Bump version code to 74

--- AndroidGarage/app/build.gradle.kts
+++ AndroidGarage/app/build.gradle.kts
@@ -58,7 +58,7 @@ android {
         applicationId = "com.chriscartland.garage"
         minSdk = 26
         targetSdk = 35
-        versionCode = 73
+        versionCode = 74
         versionName = "2.0-" + generateVersionNameTimestamp()
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
