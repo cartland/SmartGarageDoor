fa97ccc68db0f917572126e070dd778dbc0c4bda
Christopher Cartland <c.cartland@gmail.com>
2024-11-30 22:51:12 -0800

    Bump Android version code to 93

--- AndroidGarage/app/build.gradle.kts
+++ AndroidGarage/app/build.gradle.kts
@@ -59,7 +59,7 @@ android {
         applicationId = "com.chriscartland.garage"
         minSdk = 26
         targetSdk = 35
-        versionCode = 92
+        versionCode = 93
         versionName = "2.1-" + generateVersionNameTimestamp()
         setProperty("archivesBaseName", "$applicationId-$versionName-$versionCode")
 
