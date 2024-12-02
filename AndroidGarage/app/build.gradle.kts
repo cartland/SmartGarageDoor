f03e6a7381fc004d67389f9ac2025b77f9e324e1
cartland <c.cartland@gmail.com>
2024-12-02 00:35:06 -0800

    Bump Android version code to 95 and use whatsnew

--- AndroidGarage/app/build.gradle.kts
+++ AndroidGarage/app/build.gradle.kts
@@ -59,7 +59,7 @@ android {
         applicationId = "com.chriscartland.garage"
         minSdk = 26
         targetSdk = 35
-        versionCode = 94
+        versionCode = 95
         versionName = "2.1-" + generateVersionNameTimestamp()
         setProperty("archivesBaseName", "$applicationId-$versionName-$versionCode")
 
