676ea45110f7ccabd3d2d066d4ef7c8dde1b690a
cartland <c.cartland@gmail.com>
2024-12-23 00:18:27 -0800

    Create tasks to read and write

--- Embedded/main/main.c
+++ Embedded/main/main.c
@@ -7,40 +7,21 @@
 
 #define TAG "main.c"
 
-void beat_1(void *pvParameters) {
-    while (1) {  
-        ESP_LOGI(TAG, "Beat 1");
+void read_pin(void *pvParameters) {
+    while (1) {
+        ESP_LOGI(TAG, "Read pin");
         vTaskDelay(1000 / portTICK_PERIOD_MS);
     }
 }
 
-void beat_2(void *pvParameters) {
-    vTaskDelay(100 / portTICK_PERIOD_MS);
-    while (1) {  
-        ESP_LOGI(TAG, "  Beat 2");
-        vTaskDelay(2000 / portTICK_PERIOD_MS);
-    }
-}
-
-void beat_3(void *pvParameters) {
-    vTaskDelay(200 / portTICK_PERIOD_MS);
-    while (1) {  
-        ESP_LOGI(TAG, "    Beat 3");
-        vTaskDelay(3000 / portTICK_PERIOD_MS);
-    }
-}
-
-void beat_4(void *pvParameters) {
-    vTaskDelay(300 / portTICK_PERIOD_MS);
-    while (1) {  
-        ESP_LOGI(TAG, "      Beat 4");
-        vTaskDelay(4000 / portTICK_PERIOD_MS);
+void write_pin(void *pvParameters) {
+    while (1) {
+        ESP_LOGI(TAG, "Write pin");
+        vTaskDelay(5000 / portTICK_PERIOD_MS);
     }
 }
 
 void app_main(void) {
-    xTaskCreate(beat_1, "echo_task", 2048, NULL, 5, NULL);   
-    xTaskCreate(beat_2, "echo_task", 2048, NULL, 5, NULL);
-    xTaskCreate(beat_3, "echo_task", 2048, NULL, 5, NULL);
-    xTaskCreate(beat_4, "echo_task", 2048, NULL, 5, NULL);
+    xTaskCreate(read_pin, "read_pin", 2048, NULL, 5, NULL);
+    xTaskCreate(write_pin, "write_pin", 2048, NULL, 5, NULL);
 }
