e486c706824a01b1dd035039befb20dce3c471de
cartland <c.cartland@gmail.com>
2024-12-29 01:09:11 -0800

    Add a comment about the button_token

--- ESP32_FreeRTOS/components/button_token/include/button_token.h
+++ ESP32_FreeRTOS/components/button_token/include/button_token.h
@@ -6,6 +6,19 @@
 
 #include "garage_config.h"
 
+/**
+ * The purpose of the button token is to ensure that the button is pressed only when the client observes a "push button" request from the server.
+ * To simplify the memory and timing requirements, we introduce the concept of a button token, which is changed every time the server wants the client to push a button.
+ * The memory requirement is very simple -- just a small string of characters.
+ * The timing is also simple -- the client should poll the server frequently to observe a change.
+ *
+ * init: Initialize the button token.
+ * is_button_press_requested: Check if the button press is requested.
+ * consume_button_token: Consume the button token.
+ *
+ * is_button_press_requested should return true if the new_button_token is different from the button_token,
+ * but only if it is not the first button token after init(). This avoids pressing the button when the device is first powered on.
+ */
 typedef char button_token_t[MAX_BUTTON_TOKEN_LENGTH + 1];
 
 typedef struct {
