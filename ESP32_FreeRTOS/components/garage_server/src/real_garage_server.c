f201a15f7b28a38ce79e563107d4b3cd0ee32454
cartland <c.cartland@gmail.com>
2024-12-28 17:48:36 -0800

    Fix ESP_LOGI typo

--- ESP32_FreeRTOS/components/garage_server/src/real_garage_server.c
+++ ESP32_FreeRTOS/components/garage_server/src/real_garage_server.c
@@ -14,18 +14,18 @@ extern garage_server_t garage_server = {
 };
 
 void real_garage_server_init(void) {
-    ES_LOGI(TAG, "Initialize garage server");
+    ESP_LOGI(TAG, "Initialize garage server");
 }
 
 void real_garage_server_send_sensor_values(sensor_request_t *sensor_request, sensor_response_t *sensor_response) {
-    ES_LOGI(TAG, "TODO: Send sensor values to server: device_id: %s, sensor_a: %d, sensor_b: %d",
+    ESP_LOGI(TAG, "TODO: Send sensor values to server: device_id: %s, sensor_a: %d, sensor_b: %d",
              sensor_request->device_id,
              sensor_request->sensor_a,
              sensor_request->sensor_b);
 }
 
 void real_garage_server_send_button_token(button_request_t *button_request, button_response_t *button_response) {
-    ES_LOGI(TAG, "TODO: Send button token to server: device_id: %s, button_token: %s",
+    ESP_LOGI(TAG, "TODO: Send button token to server: device_id: %s, button_token: %s",
              button_request->device_id,
              button_request->button_token);
 }
