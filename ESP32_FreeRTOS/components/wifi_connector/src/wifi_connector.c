1f6806b05b1ca564cbd37ccecd736b42ef2cf5bb
cartland <c.cartland@gmail.com>
2024-12-30 01:22:06 -0800

    Set WiFi hostname

--- ESP32_FreeRTOS/components/wifi_connector/src/wifi_connector.c
+++ ESP32_FreeRTOS/components/wifi_connector/src/wifi_connector.c
@@ -1,5 +1,6 @@
 #include "esp_event.h"
 #include "esp_log.h"
+#include "esp_netif.h"
 #include "esp_wifi.h"
 #include "freertos/FreeRTOS.h"
 #include "freertos/event_groups.h"
@@ -11,6 +12,7 @@
 #define WIFI_SSID CONFIG_ESP_WIFI_SSID
 #define WIFI_PASS CONFIG_ESP_WIFI_PASSWORD
 #define WIFI_MAXIMUM_RETRY CONFIG_ESP_MAXIMUM_RETRY
+#define WIFI_HOSTNAME CONFIG_ESP_WIFI_HOSTNAME
 
 static EventGroupHandle_t s_wifi_event_group;
 
@@ -65,7 +67,11 @@ esp_err_t wifi_connector_init(void) {
 
     ESP_ERROR_CHECK(esp_netif_init());
     ESP_ERROR_CHECK(esp_event_loop_create_default());
-    esp_netif_create_default_wifi_sta();
+    esp_netif_t *sta_netif = esp_netif_create_default_wifi_sta();
+    assert(sta_netif);
+
+    // Set the hostname AFTER creating the default interface
+    esp_netif_set_hostname(sta_netif, WIFI_HOSTNAME);
 
     wifi_init_config_t cfg = WIFI_INIT_CONFIG_DEFAULT();
     ESP_ERROR_CHECK(esp_wifi_init(&cfg));
