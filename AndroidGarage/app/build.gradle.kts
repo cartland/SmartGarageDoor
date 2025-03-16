25f42c99fae48e531cbd5325d56b6b56db0eaec2
cartland <c.cartland@gmail.com>
2025-03-16 13:34:09 -0700

    Bump Android version code to 115

--- AndroidGarage/app/build.gradle.kts
+++ AndroidGarage/app/build.gradle.kts
@@ -59,7 +59,7 @@ android {
         applicationId = "com.chriscartland.garage"
         minSdk = 26
         targetSdk = 35
-        versionCode = 114
+        versionCode = 115
         versionName = "2.2-" + generateVersionNameTimestamp()
         setProperty("archivesBaseName", "$applicationId-$versionName-$versionCode")
 
